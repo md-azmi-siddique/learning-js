d5f1f6b42efcda13790de3d16d2efa15231a69de
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2023-09-13 19:53:19 +0600

    multiple variable type test

--- variable-learning.js
+++ variable-learning.js
@@ -1,5 +1,10 @@
 console.log(74)
 console.log(21);
 
-var bananaPrice = 40; 
+var bananaPrice = 40;
+var bananaPriceFloat = 40.21;
+var bananaName = 'banana name is ____';
+
 console.log(bananaPrice);
+console.log(bananaPriceFloat);
+console.log(bananaName);
