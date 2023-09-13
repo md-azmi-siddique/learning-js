4cdde2355732a9a36312933b0b50a1d3f42c8b4a
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2023-09-13 20:06:42 +0600

    varuable type

--- variable-learning.js
+++ variable-learning.js
@@ -3,14 +3,18 @@ console.log(21);
 
 var bananaPrice = 40;
 var bananaPriceFloat = 40.21;
-var bananaName = 'banana name is ____';
+var bananaName = "banana name is ____";
+var isBanana = true
+
 
 console.log(bananaPrice);
 console.log(bananaPriceFloat);
 console.log(bananaName);
+console.log(isBanana);
 
 //learing about typeof
 console.log('\n') //printing a new line
 console.log(typeof bananaPrice);
 console.log(typeof bananaPriceFloat);
 console.log(typeof bananaName);
+console.log(typeof isBanana);
