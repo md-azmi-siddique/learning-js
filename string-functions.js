fa4c836c8d7e22dec5d91d9411943b66cf079354
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2023-09-13 20:58:07 +0600

    js function

--- string-functions.js
+++ string-functions.js
@@ -3,7 +3,25 @@ var intro = "This is Azmi. I'm learning .js";
 console.log(intro.toUpperCase());
 
 //indexOf function to find any keyword inside a string
-console.log(intro.indexOf("AZMI"));
+console.log(intro.indexOf("Azmi"));
 
 //split function
 console.log(intro.split("I"));
+
+//converting string into number
+var number1 = 25;
+var number2 = 15.5;
+var number2 = parseFloat(number2); //type casting a string into number
+console.log(number1+number2)
+
+//converting a number into string
+var num = 10;
+num = ''+num;
+console.log(typeof num);
+
+//floating point
+var var1 = 0.1;
+var var2 = 0.2;
+var total = var1 + var2;
+total = total.toFixed(2); //fix the position after the decimal point
+console.log(total);
--- variable-learning.js
+++ variable-learning.js
@@ -1,14 +1,14 @@
-console.log(74)
-console.log(21);
 
 //always use camle case
 var bananaPrice = 40;
+var bananaPriceString = "35";
 var bananaPriceFloat = 40.21;
 var bananaName = "banana name is ____";
 var isBanana = true
 
 //printing variables 
 console.log(bananaPrice);
+console.log(bananaPriceString);
 console.log(bananaPriceFloat);
 console.log(bananaName);
 console.log(isBanana);
@@ -16,6 +16,7 @@ console.log(isBanana);
 //learing about typeof
 console.log('\n') //printing a new line
 console.log(typeof bananaPrice);
+console.log(typeof bananaPriceString);
 console.log(typeof bananaPriceFloat);
 console.log(typeof bananaName);
 console.log(typeof isBanana);
